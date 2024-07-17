74eb2e73eea166eafed994212d84bae11ae75681
Som <54392956+SomBD@users.noreply.github.com>
2024-07-17 17:28:41 +0530

    http resquest changed to https

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -2,5 +2,5 @@ export class AppConstants {
     // if env local 
     //public static readonly baseURL: string = 'http://localhost:8001';
     // if env other
-    public static readonly baseURL: string = 'http://13.127.168.216:8001';  
+    public static readonly baseURL: string = 'https://13.127.168.216:8001';  
 }
